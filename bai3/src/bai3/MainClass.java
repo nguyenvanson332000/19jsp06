a049236694121e4db33a3ffa15c6efb41db27280
Phuc Le <ltphuc5594@gmail.com>
2019-12-30 18:31:53 +0700

    Add think java book

--- bai3/src/bai3/MainClass.java
+++ bai3/src/bai3/MainClass.java
@@ -1,10 +1,11 @@
 package bai3;
 
 public class MainClass {
+
 	public static void main(String[] args) {
-		 ThiSinh thiSinh = new ThiSinh("1", "A", "B", 1, KhoiThi.KHOI_A);
-		 System.out.println(thiSinh.getKhoiThi());
-		 System.out.println(thiSinh.getKhoiThi().getDsMon());
+		ThiSinh thiSinh = new ThiSinh("1", "A", "B", 1, KhoiThi.KHOI_A);
+		System.out.println(thiSinh.getKhoiThi());
+		System.out.println(thiSinh.getKhoiThi().getDsMon());
 	}
 
 	static void f1(int[] a) {
---
+++ bai3/src/bai3/MonthUtil.java
@@ -0,0 +1,7 @@
+package bai3;
+
+public class MonthUtil {
+	public static int getDayOfMonth(int month) {
+		return 30;
+	}
+}
